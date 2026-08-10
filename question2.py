86f0218efe3e4f46c21f5cff09f7e0c6b26fa753
R Kiran Kumar Reddy <kiran99120948@gmail.com>
2026-08-10 19:52:34 +0530

    Completed question -2

---
+++ question2.py
@@ -0,0 +1,26 @@
+customer_name = input("Enter customer name: ")
+age = int(input("Enter age: "))
+tickets = int(input("Enter number of tickets: "))
+
+if age < 12:
+    ticket_price = 120
+elif age <= 59:
+    ticket_price = 200
+else:
+    ticket_price = 150
+
+total_before_discount = ticket_price * tickets
+
+if tickets >= 5:
+    discount = total_before_discount * 0.10
+else:
+    discount = 0
+
+final_amount = total_before_discount - discount
+
+print("Customer Name:", customer_name)
+print("Ticket Price: ₹", ticket_price)
+print("Number of Tickets:", tickets)
+print("Total Before Discount: ₹", total_before_discount)
+print("Discount: ₹", discount)
+print("Final Amount: ₹", final_amount)
